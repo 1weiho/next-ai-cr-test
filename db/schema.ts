5022030bb511ef7d3273addfa10900fab26d1f06
何奕瑋 <david.ho@program.com.tw>
2025-04-01 10:38:18 +0800

    chore: drizzle config and todo schema

---
+++ db/schema.ts
@@ -0,0 +1,10 @@
+import { boolean, integer, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const todosTable = pgTable("todos", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 255 }).notNull(),
+  description: text(),
+  completed: boolean().notNull().default(false),
+  createdAt: timestamp().notNull().defaultNow(),
+  updatedAt: timestamp().notNull().$onUpdate(() => new Date()),
+});
