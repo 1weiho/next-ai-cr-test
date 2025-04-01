6a60f8e8857328f7756033c36eec0fab869bd903
何奕瑋 <david.ho@program.com.tw>
2025-04-01 13:59:22 +0800

    feat: todo due date

--- db/schema.ts
+++ db/schema.ts
@@ -1,12 +1,15 @@
-import { boolean, integer, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import { boolean, integer, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core"
 
 export const todosTable = pgTable("todos", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   title: varchar({ length: 255 }).notNull(),
   description: text(),
   completed: boolean().notNull().default(false),
+  dueDate: timestamp(),
   createdAt: timestamp().notNull().defaultNow(),
-  updatedAt: timestamp().notNull().$onUpdate(() => new Date()),
-});
+  updatedAt: timestamp()
+    .notNull()
+    .$onUpdate(() => new Date()),
+})
 
 export type Todo = typeof todosTable.$inferSelect
